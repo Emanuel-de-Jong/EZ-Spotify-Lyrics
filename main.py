23d13a9bfa2f0178e2e033cf0426aa58bb240f59
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-10-15 15:15:04 +0200

    Download lrc or get cached

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinterdnd2 import DND_TEXT, TkinterDnD
 import requests
@@ -8,6 +9,9 @@ import syncedlyrics
 BLACKLISTED_TITLE_WORDS = [
     "remix", "mix", "live", "extended", "radio", "edit", "version", "feat", "ft", "featuring", "asot"
 ]
+
+LYRICS_PATH = "Lyrics"
+SHOULD_SAVE_LYRICS = True
 
 root = TkinterDnD.Tk()
 root.title("EZ Spotify Lyrics")
@@ -28,15 +32,23 @@ def write(text):
     text_box.insert(tk.END, text)
 
 def get_lyrics(url):
-    artists, title = get_song_info(url)
-    print(artists)
-    print(title)
+    lyrics = None
+    existing_lyrics_path = get_lyrics_dir_path(url)
+    if os.path.exists(existing_lyrics_path):
+        lyrics = get_existing_lyrics(existing_lyrics_path)
+    
+    if not lyrics:
+        artists, title = get_song_info(url)
+        lyrics = download_lyrics(artists, title, url)
+        if not lyrics:
+            return
+
+    print(lyrics)
 
 def get_song_info(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     page_title = soup.title.string
-    print(page_title)
 
     split_str = " song by "
     if " and lyrics " in page_title:
@@ -51,6 +63,37 @@ def get_song_info(url):
     artists = artist_str.split(" | ")[0].split(", ")
 
     return artists, title
+
+def download_lyrics(artists, title, url):
+    search_query = f"{title} {' '.join(artists)}"
+    
+    save_path = None
+    if SHOULD_SAVE_LYRICS:
+        save_path = get_lyrics_dir_path(url)
+        os.makedirs(save_path, exist_ok=True)
+        save_path += "{search_term}.lrc"
+
+    lyrics = syncedlyrics.search(search_query, save_path=save_path)
+    if not lyrics:
+        write("Lyrics not found.")
+        return
+    
+    return lyrics
+
+def get_lyrics_dir_path(url):
+    dir_name = url.split('/')[-1]
+    return f"{LYRICS_PATH}{os.sep}{dir_name}{os.sep}"
+
+def get_existing_lyrics(dir_path):
+    file_path = None
+    for file_name in os.listdir(dir_path):
+        if file_name.endswith('.lrc'):
+            file_path = os.path.join(dir_path, file_name)
+    if not file_path:
+        return
+    
+    with open(file_path, 'r') as file:
+        return file.read()
 
 if __name__ == "__main__":
     root.mainloop()
